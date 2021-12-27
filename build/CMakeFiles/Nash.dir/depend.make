26359cdfec431dfdeff253c6d0319ec56ab2e541
LukeChemeriov <dgm.bailey@outlook.com>
2021-12-27 17:58:28 +0300

    Removed CMake things

--- build/CMakeFiles/Nash.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Nash.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chemeriov/dev/Source/Nash
-# Build directory: /home/chemeriov/dev/Source/Nash/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
