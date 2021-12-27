811006a972d7fda09e108a286e98190e12a23934
LukeChemeriov <dgm.bailey@outlook.com>
2021-12-27 17:42:49 +0300

    Inital commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chemeriov/dev/Source/Nash
+# Build directory: /home/chemeriov/dev/Source/Nash/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
